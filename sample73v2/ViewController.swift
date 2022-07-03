0025dc8bb632e463c6cc6d886c93f84b4ed75d2f
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2022-07-03 14:39:51 +0900

    Initial Commit

---
+++ sample73v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sample73v2
+//
+//  Created by AnzaiYasuto al18011 on 2022/07/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
